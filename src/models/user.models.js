768c3956ee5a2b1cd6ef3704b89a7021651749c8
0007ishav <2002ishavs@gmail.com>
2024-03-25 23:11:56 +0530

    Added User & Video Model

---
+++ src/models/user.models.js
@@ -0,0 +1,89 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken";
+
+const userSchema = new Schema({
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    index: true,    // For searching mark it true for optmized approach
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  fullName: {
+    type: String,
+    required: true,
+    trim: true,
+    index: true
+  },
+  avatar: {
+    type: String,
+    required : true,
+  },
+  coverImage: {
+    type: String,
+  },
+  watchHistory: [
+    {
+        type: Schema.Types.ObjectId,
+        ref: "Video"
+    }
+  ],
+  password: {
+    type: String,
+    required: [true, "Password is required"]
+  },
+  refreshToken: {
+    type: String,
+  },
+
+},{timestamps: true});
+
+// in pre hook dont use arrow function bcuz it doesnt have "this" reference
+userSchema.pre("save", async function(next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = bcrypt.hash(this.password, 10)
+  next();
+})
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+ return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      userName: this.userName,
+      fullName: this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+  )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+  )
+}
+
+export const User = mongoose.model("User", userSchema);
