73f205d7f261e3305a446a4a69c1d50440c67bc3
0007ishav <2002ishavs@gmail.com>
2024-03-29 02:55:11 +0530

    add refreshAccessToken endpoint

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,18 +1,18 @@
-import { ApiError } from "../utils/ApiError";
-import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken"
 import { User } from "../models/user.models.js"
 
 // req & next is used but "res" is not used , we can just write "_" instead of "res"
 export const verifyJWT = asyncHandler(async (req, _, next) => {
    try {
-    const token = req.cookie?.accessToken || req.header("Authorozation")?.replace("Bearer ", "")
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
  
     if (!token) {
          throw new ApiError(401, "Unauthorized request")
     }
  
-    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEM_SECRET)
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
  
     const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
  
